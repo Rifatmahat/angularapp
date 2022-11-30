6cfb3fac73184d5a09d6f64a3367b87893de22c3
Rifatmahat <rifatmahat97@gmail.com>
2022-11-30 18:17:45 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { DirectivedemoComponent } from './directivedemo/directivedemo.component';
+import { NgswitchComponent } from './ngswitch/ngswitch.component';
+import { DirectiveAssignmentComponent } from './directive-assignment/directive-assignment.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    SuccessalertComponent,
+    WarningAlertComponent,
+    HeaderComponent,
+    DirectivedemoComponent,
+    NgswitchComponent,
+    DirectiveAssignmentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
